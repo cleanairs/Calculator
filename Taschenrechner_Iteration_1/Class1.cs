f67994ed58dfc8146654d582538d62fe0cf1a2e5
julian helmholz <julian.helmholz@gmx.de>
2018-03-07 10:50:01 +0100

    Class float

---
+++ Taschenrechner_Iteration_1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner_Iteration_1
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            float pi =3
+        }
+
+    }
+}
