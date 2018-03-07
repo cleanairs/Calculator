15af422fa727062c472f4e08a61740b97ac00867
julian helmholz <julian.helmholz@gmx.de>
2018-03-07 10:14:35 +0100

    Initialer Commit

---
+++ Taschenrechner_Iteration_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Taschenrechner_Iteration_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Story "Addieren" : Als Benutzer möchte ich den Taschenrechner schnell aufrufen können um mein Resultat zu bekommen.
+            Console.Write("Bitte gib die erste Zahl ein und bestätige mit Enter:");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib die zweite Zahl ein und bestätige mit Enter:");
+            string zweiterSummand = Console.ReadLine();
+
+            // Wandlung string -> int (Text in Ganzzahl)
+            int ersterSummandAlsZahl = Convert.ToInt32(ersterSummand);
+            int zweiterSummandAlsZahl = Convert.ToInt32(zweiterSummand);
+
+            Console.WriteLine("Gewandelter erster Summand: {0}", ersterSummandAlsZahl);
+            Console.WriteLine("Gewandelter zweiter Summand: {0}", zweiterSummandAlsZahl);
+
+            //Berechnung
+            int Ergebnis = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            //Ausgabe
+            Console.WriteLine("Das Ergebnis lautet: " + Ergebnis);
+            Console.WriteLine("Enter drücken bitte. :)");
+                        Console.ReadLine();
+
+
+        }
+    }
+}
