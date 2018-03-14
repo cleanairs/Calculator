ecb644d8dab1601507bbdaf0d6b7c6d78f259921
julian helmholz <julian.helmholz@gmx.de>
2018-03-14 11:13:21 +0100

    Methode ausgelagert Addiere

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -15,17 +15,24 @@ namespace Taschenrechner
             // Wandlung string -> int (Text in Ganzzahl)
             double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
             double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
-             
+
             //Berechnung
-            double Ergebnis = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+            double summe = Addiere(ersterSummandAlsZahl, zweiterSummandAlsZahl );
 
-            //Ausgabevbnvbn
-            Console.WriteLine("Das Ergebnis lautet: " + Ergebnis);
-            Console.WriteLine("Enter drücken bitte. :)");
-
-            Console.ReadLine();
-
-
+            //Ausgabe
+            Console.WriteLine("Das Ergebnis ist: " + summe);
+            WarteAufBenutzerEingabe();
         }
+        static void WarteAufBenutzerEingabe()
+        {
+            Console.WriteLine("Enter drücken bitte. :)");
+            Console.ReadLine();
+        }
+        static double Addiere(double ersterSummandAlsZahl, double zweiterSummandAlsZahl)
+        {
+           double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+           return summe;
+        }
+
     }
 }
