1f07efdebef67dee540fd2099738366a7e3068fb
julian helmholz <julian.helmholz@gmx.de>
2018-03-16 10:59:58 +0100

    abfrage +- eingefügt

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -7,28 +7,23 @@ namespace Taschenrechner
         static void Main(string[] args)
         {
             // User Story "Addieren" : Als Benutzer möchte ich den Taschenrechner schnell aufrufen können um mein Resultat zu bekommen.
-            string ersterSummand = HoleSummanden("Bitte gib die erste Zahl ein und bestätige mit Enter:");
-            string zweiterSummand = HoleSummanden("Bitte gib die zweite Zahl ein und bestätige mit Enter:");
-
+            string erstZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein und bestätige mit Enter:");
+            string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein und bestätige mit Enter:");
+            string operation = HoleBenutzerEingabe("Bitte gib für Addition + und für Substraktion - ein: ");
+            
             // Wandlung string -> int (Text in Ganzzahl)
             //TODO: Auslagern in Methode wenn Struktur umfangereicher geworden ist.
-            double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
-            double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
+            double ersteZahlAlsDouble = Convert.ToDouble(erstZahlAlsString);
+            double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
 
-            //Berechnung
-            double summe = Addiere(ersterSummandAlsZahl, zweiterSummandAlsZahl );
-            double differenz = Subtrahiere(ersterSummandAlsZahl, zweiterSummandAlsZahl);
+            //Berechnung            
+            double summe = Addiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
+            double differenz = Subtrahiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
 
             //Ausgabe
             Console.WriteLine("Die Summe ist: " + summe);
             Console.WriteLine("Die Differenz ist: " + differenz);
-            WarteAufBenutzerEingabe();
-        }
-        
-        static void WarteAufBenutzerEingabe()
-        {
-            Console.WriteLine("Enter drücken bitte. :)");
-            Console.ReadLine();
+            HoleBenutzerEingabe("Enter drücken bitte. :)");
         }
 
         static double Subtrahiere(double minuend, double subtrahent)
@@ -36,18 +31,18 @@ namespace Taschenrechner
             double differenz = minuend - subtrahent;
             return differenz;
         }
-        
+
         static double Addiere(double ersterSummandAlsZahl, double zweiterSummandAlsZahl)
         {
-           double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
-           return summe;
+            double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+            return summe;
         }
 
-        static string HoleSummanden(string ausgabeText)
+        static string HoleBenutzerEingabe(string ausgabeText)
         {
             Console.Write(ausgabeText);
-            string summand = Console.ReadLine();
-            return summand;
+            string Eingabe = Console.ReadLine();
+            return Eingabe;
         }
     }
 }
