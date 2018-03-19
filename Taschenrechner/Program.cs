a0b689edd3aab7593404aba3983bb47e5007473b
julian helmholz <julian.helmholz@gmx.de>
2018-03-19 18:49:17 +0100

    if else +-

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -6,24 +6,31 @@ namespace Taschenrechner
     {
         static void Main(string[] args)
         {
-            // User Story "Addieren" : Als Benutzer möchte ich den Taschenrechner schnell aufrufen können um mein Resultat zu bekommen.
-            string erstZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein und bestätige mit Enter:");
-            string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein und bestätige mit Enter:");
-            string operation = HoleBenutzerEingabe("Bitte gib für Addition + und für Substraktion - ein: ");
+        // User Story "Addieren" : Als Benutzer möchte ich den Taschenrechner schnell aufrufen können um mein Resultat zu bekommen.
+        string erstZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein und bestätige mit Enter:");
+        string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein und bestätige mit Enter:");
+        string operation = HoleBenutzerEingabe("Bitte gib für Addition + und für Substraktion - ein: ");
             
-            // Wandlung string -> int (Text in Ganzzahl)
-            //TODO: Auslagern in Methode wenn Struktur umfangereicher geworden ist.
-            double ersteZahlAlsDouble = Convert.ToDouble(erstZahlAlsString);
-            double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
+        // Wandlung string -> int (Text in Ganzzahl)
+        //TODO: Auslagern in Methode wenn Struktur umfangereicher geworden ist.
+        double ersteZahlAlsDouble = Convert.ToDouble(erstZahlAlsString);
+        double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
 
-            //Berechnung            
+        //Berechnung            
+        if (operation == "+")
+	    {
             double summe = Addiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
+	    }else
+	    {
+            double differenz = Subtrahiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
+	    }
+            
             double differenz = Subtrahiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
 
-            //Ausgabe
-            Console.WriteLine("Die Summe ist: " + summe);
-            Console.WriteLine("Die Differenz ist: " + differenz);
-            HoleBenutzerEingabe("Enter drücken bitte. :)");
+        //Ausgabe
+        Console.WriteLine("Die Summe ist: " + summe);
+        Console.WriteLine("Die Differenz ist: " + differenz);
+        HoleBenutzerEingabe("Enter drücken bitte. :)");
         }
 
         static double Subtrahiere(double minuend, double subtrahent)
