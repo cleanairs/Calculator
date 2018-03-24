bfc58e0d912661318190aba6b10519c138a93c2b
julian helmholz <julian.helmholz@gmx.de>
2018-03-24 14:15:57 +0100

    berechnung ausgelagert

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -10,87 +10,49 @@ namespace Taschenrechner
             string erstZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein und bestätige mit Enter:");
             string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein und bestätige mit Enter:");
             string operation = HoleBenutzerEingabe("Bitte gib +, -, / oder * ein: ");
-
+            
             // Wandlung string -> int (Text in Ganzzahl)
             //TODO: Auslagern in Methode wenn Struktur umfangereicher geworden ist.
             double ersteZahlAlsDouble = Convert.ToDouble(erstZahlAlsString);
             double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
+            double resultat = Berechnung(operation, ersteZahlAlsDouble, zweiteZahlAlsDouble);
 
-            //Berechnung            
-            double resultat;
+            Console.WriteLine(resultat);
+            HoleBenutzerEingabe("Zum beenden Enter drücker!");
+        }
+
+        private static double Berechnung(string operation, double ersteZahl, double zweiteZahl)
+        {double resultat = 0;
             switch (operation)
             {
                 case "+":
-                    resultat = Addiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
-                    Console.WriteLine("Die Summe ist: " + resultat);
+                    double summe = ersteZahl + zweiteZahl;
+                    return summe;
                     break;
                 case "-":
-                    resultat = Subtrahiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
-                    Console.WriteLine("Die Differenz ist: " + resultat);
+                    double differenz = ersteZahl - zweiteZahl;
+                    return differenz;
                     break;
                 case "*":
-                    resultat = Multipliziere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
-                    Console.WriteLine("Das Produkt ist: " + resultat);
+                    double produkt = ersteZahl * zweiteZahl;
+                    return produkt;
                     break;
                 case "/":
-                    resultat = Dividiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
-                    Console.WriteLine("Der Quotient ist: " + resultat);
+                    double quotient = ersteZahl / zweiteZahl;
+                    return quotient;
                     break;
                 default:
                     Console.WriteLine("Bitte +, /, * oder - eingeben!");
                     break;
-            }
-
-
-            /*    if (operation == "+")
-            {
-                resultat = Addiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
-                Console.WriteLine("Die Summe ist: " + resultat);
-                }
-            else if (operation == "-")
-                {
-                resultat = Subtrahiere(ersteZahlAlsDouble, zweiteZahlAlsDouble);
-                Console.WriteLine("Die Differenz ist: " + resultat);
-                }
-            else if (true)
-                {
-
-                }
-            else
-                {
-                Console.WriteLine("Bitte + oder - eingeben!");
-                }
-            */
-
-            HoleBenutzerEingabe("Zum beenden Enter drücker!");
+            }return resultat;
         }
-
-        static double Subtrahiere(double minuend, double subtrahent)
-        {
-            double differenz = minuend - subtrahent;
-            return differenz;
-        }
-
-        static double Addiere(double ersterSummandAlsZahl, double zweiterSummandAlsZahl)
-        {
-            double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
-            return summe;
-        }
-        static double Multipliziere(double ersterMultiplikator, double zweiterMultiplikator)
-        {
-            double produkt = ersterMultiplikator * zweiterMultiplikator;
-            return produkt;
-        }
-        public static double Dividiere(double ersterTeiler, double zweiterTeiler)
-        {
-            double quotient = ersterTeiler / zweiterTeiler;
-            return quotient;
-        }
-        static string HoleBenutzerEingabe(string ausgabeText)
+        private static string HoleBenutzerEingabe(string ausgabeText)
         {
             Console.Write(ausgabeText);
             string Eingabe = Console.ReadLine();
             return Eingabe;
         }
+              
+        
     }
 }
